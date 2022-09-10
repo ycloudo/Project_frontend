46964b95f980ed1a0c30ecb761299730b9939a3a
chiwen <bjbwendy2000@gmail.com>
2022-09-10 15:12:52 +0800

    0910eatpage

--- Pages/Eat.js
+++ Pages/Eat.js
@@ -3,13 +3,192 @@ import { View, Text, Image, StyleSheet, StatusBar, Animated,TouchableOpacity } f
 import WheelOfFortune from 'react-native-wheel-of-fortune';
 import AnimatedFade from '../Components/AnimatedFade';
 
-const Eat = () => {
+const participants = [
+    '1',
+    '2',
+    '3',
+    '4',
+    '5',
+    '6',
+    '7',
+    '8',
+    '9',
+  ];
+  
+  class Eat extends Component {
+    
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+        winnerValue: null,
+        winnerIndex: null,
+        started: false,
+        activeIndex: 0,
+        startbutton: 0,
+      };
+      this.child = null;
+    }
+    buttonPress = () => {
+      this.setState({
+        started: true,
+      });
+      this.child._onPress();
+    };
+    
+    render() {
+      
+    const wheelOptions = {
+        rewards: participants,
+        knobSize: 30,
+        borderWidth: 10,
+        borderColor: '#fff',
+        innerRadius: 30,
+        duration: 100,
+        backgroundColor: 'transparent',
+        textAngle: 'horizontal',
+        knobSource: require('./down-arrow.png'),
+        onRef: ref => (this.child = ref),
+    };
+    
+    var touchProps = {
+        activeOpacity: 0.6,
+        onPress: () => console.log('press'),
+      }; 
+    var starttouchProps = {
+        activeOpacity: 1,
+        //onPress: () => console.log('press'),
+        onPress: () => {this.setState({ startbutton: 1 })},
+      };       
     return (
-        <View style={styles.main_container}>
-            <Text>Eat</Text>
+        <View style={styles.scrollview_container}>
+          
+            <View style={styles.top}>
+            <AnimatedFade>
+                <Text style={styles.top1}>今天</Text>
+                <Text style={styles.top2}>要吃什麼呢？</Text>
+            </AnimatedFade>
+                <View style={styles.choice_container}>
+                    <View style={styles.boxf}>
+                      <TouchableOpacity {...touchProps}
+                      onPress={() => {
+                        this.setState({ activeIndex: 0, started: false, winnerValue: null, winnerIndex: null });
+                        this.child.resetWheelState();
+                        this.child.prepareWheel();
+                        this.child.angleListener();
+                      }}
+                      >
+                          <View style={this.state.activeIndex === 0 ? styles.optionActive : styles.option}>
+                              <Image
+                                  source={require("../assets/money.png")}
+                                  style={styles.photo}
+                              />
+                              <Text style={styles.text}>中式</Text>
+                          </View>
+                      </TouchableOpacity>
+                    </View>
+                    <View style={styles.box}>
+                      <TouchableOpacity {...touchProps}
+                      onPress={() => {
+                        this.setState({ activeIndex: 1, started: false, winnerValue: null, winnerIndex: null });
+                        this.child.resetWheelState();
+                        this.child.prepareWheel();
+                        this.child.angleListener();
+                      }}
+                      >
+                          <View style={this.state.activeIndex === 1 ? styles.optionActive : styles.option}>
+                              <Image
+                                  source={require("../assets/service.png")}
+                                  style={styles.photo}
+                              />
+                              <Text style={styles.text}>日式</Text>
+                          </View>
+                      </TouchableOpacity>
+                    </View>
+                    <View style={styles.box}>
+                      <TouchableOpacity {...touchProps}
+                      onPress={() => {
+                        this.setState({ activeIndex: 2, started: false, winnerValue: null, winnerIndex: null });
+                        this.child.resetWheelState();
+                        this.child.prepareWheel();
+                        this.child.angleListener();
+                      }}
+                      >
+                          <View style={this.state.activeIndex === 2 ? styles.optionActive : styles.option}>
+                              <Image
+                                  source={require("../assets/food.png")}
+                                  style={styles.photo}
+                              />
+                              <Text style={styles.text}>韓式</Text>
+                          </View>
+                      </TouchableOpacity>
+                    </View>
+                    <View style={styles.box}>
+                      <TouchableOpacity {...touchProps}
+                      onPress={() => {
+                        this.setState({ activeIndex: 3, started: false, winnerValue: null, winnerIndex: null });
+                        this.child.resetWheelState();
+                        this.child.prepareWheel();
+                        this.child.angleListener();
+                      }}
+                      >
+                          <View style={this.state.activeIndex === 3 ? styles.optionActive : styles.option}>
+                              <Image
+                                  source={require("../assets/money.png")}
+                                  style={styles.photo}
+                              />
+                              <Text style={styles.text}>美式</Text>
+                          </View>
+                      </TouchableOpacity>
+                    </View>
+                </View>
+            </View>
+            
+            
+          <View style={styles.container}>
+          <StatusBar barStyle={'light-content'} />
+         
+          <WheelOfFortune
+            options={wheelOptions}
+            getWinner={(value, index) => {
+              this.setState({winnerValue: value, winnerIndex: index});
+            }}
+          />
+          {!this.state.started && (
+            <View style={styles.startButtonView}>
+            <View style={styles.startButtonback}>
+              <TouchableOpacity {...starttouchProps}
+                onPress={() => this.buttonPress()}
+                style={this.state.startbutton === 1 ? styles.startButtonActive : styles.startButton}
+                >
+                <Text style={styles.startButtonText}>START!</Text>
+              </TouchableOpacity>
+              </View>
+            </View>
+          )}
+          
+          {this.state.winnerIndex != null && (
+            <View style={styles.winnerView}>
+              <Text style={styles.winnerText}>
+                Lets eat {participants[this.state.winnerIndex]}
+              </Text>
+              <TouchableOpacity
+                onPress={() => {
+                  this.setState({winnerIndex: null});
+                  this.child._tryAgain();
+                }}
+                style={styles.tryAgainButton}>
+                <Text style={styles.tryAgainText}>TRY AGAIN</Text>
+              </TouchableOpacity>
+            </View>
+          )}
         </View>
-    );
-};
+            </View>
+        
+        
+      );
+    }
+  }
 
 const styles = StyleSheet.create({
     scrollview_container: {
