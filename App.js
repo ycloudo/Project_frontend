2702f778545bace20f149d8bc965eb2c954b03b4
ycloudo <cces994512@gmail.com>
2022-11-03 22:34:27 +0800

    concat signup with favor setting

--- App.js
+++ App.js
@@ -1,175 +1,211 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
-import { useState, useMemo, useEffect, useReducer } from "react";
-import "react-native-gesture-handler";
-import { NavigationContainer } from "@react-navigation/native";
-import Drawer from "./routes/DrawerNav";
-import Auth from "./routes/AuthStackNav";
-import { AuthContext } from "./content/AuthContext";
-import { API_URL } from "@env";
-import * as SecureStore from "expo-secure-store";
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import { useState, useMemo, useEffect, useReducer } from 'react';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
+import Drawer from './routes/DrawerNav';
+import Auth from './routes/AuthStackNav';
+import { AuthContext } from './content/AuthContext';
+import { API_URL } from '@env';
+import * as SecureStore from 'expo-secure-store';
+import FavorSettingStack from './routes/FavorSettingStack';
 
 export default function App() {
-    const initialLoginState = {
-        isLoading: true,
-        userId: null,
-        userToken: null,
-    };
+  const initialLoginState = {
+    isLoading: true,
+    userId: null,
+    userToken: null,
+    everWroteFavor: false,
+  };
 
-    const authReducer = (state, action) => {
-        //狀態
-        switch (action.type) {
-            case "LOADING":
-                return {
-                    ...state,
-                    userId: action.id,
-                    userToken: action.token,
-                    isLoading: false,
-                };
-            case "LOGIN":
-                return {
-                    ...state,
-                    userId: action.id,
-                    userToken: action.token,
-                    isLoading: false,
-                };
-            case "LOGOUT":
-                return {
-                    ...state,
-                    userId: null,
-                    userToken: null,
-                    isLoading: false,
-                };
-            case "SIGNUP":
-                return {
-                    ...state,
-                    userId: action.id,
-                    userToken: action.token,
-                    isLoading: false,
-                };
-        }
-    };
-
-    const [Authstate, dispatch] = useReducer(authReducer, initialLoginState);
-
-    const authContext = useMemo(() => ({
-        login: async (account, password) => {
-            //api call
-            const requestOption = {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({
-                    account: account,
-                    password: password,
-                }),
-            };
-            let userToken, userId, res;
-            try {
-                res = await fetch(`${API_URL}/user/login`, requestOption).then(
-                    (r) => r.json()
-                );
-                userId = res.uid;
-                userToken = res.token;
-            } catch (e) {
-                console.error(e);
-            }
-            if (userId && userToken) {
-                await SecureStore.setItemAsync("auth-token", userToken);
-                await SecureStore.setItemAsync("user-id", userId);
-            }
-            setTimeout(() => {
-                dispatch({
-                    type: "LOGIN",
-                    id: userId,
-                    token: userToken,
-                });
-            }, 280);
-            return res;
-        },
-        logout: async () => {
-            await SecureStore.deleteItemAsync("auth-token");
-            await SecureStore.deleteItemAsync("user-id");
-            dispatch({ type: "LOGOUT" });
-        },
-        signup: async (name, account, password) => {
-            //api call
-            const requestOption = {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({
-                    name: name,
-                    account: account,
-                    password: password,
-                }),
-            };
-            let userToken, userId;
-            try {
-                const res = await fetch(
-                    `${API_URL}/user/register`,
-                    requestOption
-                );
-                const json = await res.json();
-                userId = json.info._id;
-                userToken = json.token;
-            } catch (e) {
-                console.error(e);
-            }
-            dispatch({
-                type: "SIGNUP",
-                id: userId,
-                token: userToken,
-            });
-        },
-    }));
-    useEffect(() => {
-        const fetchUser = async () => {
-            let token = await SecureStore.getItemAsync("auth-token");
-            let userId = null;
-            const requestOption = {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify({
-                    token: token,
-                }),
-            };
-            const response = await fetch(
-                `${API_URL}/user/isTokenValid`,
-                requestOption
-            ).then((res) => res.json());
-            if (response) {
-                userId = await SecureStore.getItemAsync("user-id");
-            } else {
-                await SecureStore.deleteItemAsync("auth-token");
-                await SecureStore.deleteItemAsync("user-id");
-                token = null;
-            }
-            setTimeout(() => {
-                dispatch({ type: "LOADING", token: token, id: userId });
-            }, 1000);
+  const authReducer = (state, action) => {
+    //狀態
+    switch (action.type) {
+      case 'LOADING':
+        return {
+          ...state,
+          userId: action.id,
+          userToken: action.token,
+          isLoading: false,
         };
-        fetchUser();
-    }, []);
-    if (Authstate.isLoading) {
-        return (
-            <View style={styles.container}>
-                <ActivityIndicator size="large" />
-            </View>
-        );
+      case 'LOGIN':
+        return {
+          ...state,
+          userId: action.id,
+          userToken: action.token,
+          isLoading: false,
+          everWroteFavor: true,
+        };
+      case 'LOGOUT':
+        return {
+          ...state,
+          userId: null,
+          userToken: null,
+          isLoading: false,
+          everWroteFavor: false,
+        };
+      case 'SIGNUP':
+        return {
+          ...state,
+          userId: action.id,
+          userToken: action.token,
+          isLoading: false,
+        };
+      case 'FAVOR':
+        return {
+          ...state,
+          everWroteFavor: true,
+        };
     }
+  };
+
+  const [Authstate, dispatch] = useReducer(authReducer, initialLoginState);
+
+  const authContext = useMemo(() => ({
+    login: async (account, password) => {
+      //api call
+      const requestOption = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          account: account,
+          password: password,
+        }),
+      };
+      let userToken, userId, res;
+      try {
+        res = await fetch(`${API_URL}/user/login`, requestOption).then((r) =>
+          r.json()
+        );
+        userId = res.uid;
+        userToken = res.token;
+      } catch (e) {
+        console.error(e);
+      }
+      if (userId && userToken) {
+        await SecureStore.setItemAsync('auth-token', userToken);
+        await SecureStore.setItemAsync('user-id', userId);
+      }
+      setTimeout(() => {
+        dispatch({
+          type: 'LOGIN',
+          id: userId,
+          token: userToken,
+        });
+      }, 280);
+      return res;
+    },
+    logout: async () => {
+      await SecureStore.deleteItemAsync('auth-token');
+      await SecureStore.deleteItemAsync('user-id');
+      dispatch({ type: 'LOGOUT' });
+    },
+    signup: async (name, account, password) => {
+      //api call
+      const requestOption = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          name: name,
+          account: account,
+          password: password,
+        }),
+      };
+      let userToken, userId;
+      try {
+        const res = await fetch(`${API_URL}/user/register`, requestOption);
+        const json = await res.json();
+        userId = json.info._id;
+        userToken = json.token;
+      } catch (e) {
+        console.error(e);
+      }
+      dispatch({
+        type: 'SIGNUP',
+        id: userId,
+        token: userToken,
+      });
+    },
+    wroteFavor: () => {
+      dispatch({
+        type: 'FAVOR',
+      });
+    },
+  }));
+  useEffect(() => {
+    const fetchUser = async () => {
+      let token = await SecureStore.getItemAsync('auth-token');
+      let userId = null;
+      const requestOption = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+          token: token,
+        }),
+      };
+      const response = await fetch(
+        `${API_URL}/user/isTokenValid`,
+        requestOption
+      ).then((res) => res.json());
+      if (response) {
+        userId = await SecureStore.getItemAsync('user-id');
+      } else {
+        await SecureStore.deleteItemAsync('auth-token');
+        await SecureStore.deleteItemAsync('user-id');
+        token = null;
+      }
+      setTimeout(() => {
+        dispatch({
+          type: 'LOADING',
+          token: token,
+          id: userId,
+        });
+      }, 1000);
+    };
+    fetchUser();
+  }, []);
+  if (Authstate.isLoading) {
     return (
-        <AuthContext.Provider value={authContext}>
-            <StatusBar style="auto" />
-            <NavigationContainer>
-                {Authstate.userToken != null ? <Drawer /> : <Auth />}
-            </NavigationContainer>
-        </AuthContext.Provider>
+      <View style={styles.container}>
+        <ActivityIndicator size="large" />
+      </View>
     );
+  }
+  if (Authstate.userToken == null) {
+    return (
+      <AuthContext.Provider value={authContext}>
+        <StatusBar style="auto" />
+        <NavigationContainer>
+          <Auth />
+        </NavigationContainer>
+      </AuthContext.Provider>
+    );
+  }
+  if (!Authstate.everWroteFavor) {
+    return (
+      <AuthContext.Provider value={authContext}>
+        <StatusBar style="auto" />
+        <NavigationContainer>
+          <FavorSettingStack />
+        </NavigationContainer>
+      </AuthContext.Provider>
+    );
+  } else {
+    return (
+      <AuthContext.Provider value={authContext}>
+        <StatusBar style="auto" />
+        <NavigationContainer>
+          <Drawer />
+        </NavigationContainer>
+      </AuthContext.Provider>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-    },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
--- routes/FavorSettingStack.js
+++ routes/FavorSettingStack.js
@@ -1,31 +1,35 @@
-import React from "react";
-import { createStackNavigator } from "@react-navigation/stack";
-import FavorSetting from "../Pages/FavorSetting";
-import FavorSetting2 from "../Pages/FavorSetting2";
-import DrawerNav from "./DrawerNav";
-import FavorSetting0 from "../Pages/FavorSetting0";
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import FavorSetting from '../Pages/FavorSetting';
+import FavorSetting2 from '../Pages/FavorSetting2';
+import FavorSetting0 from '../Pages/FavorSetting0';
+import Navbar from './Navbar';
 
 const Stack = createStackNavigator();
 
 const FavorSettingStack = () => {
-    return (
-        <Stack.Navigator
-            screenOptions={{
-                header: () => null,
-                gestureEnabled: false,
-            }}
-            initialRouteName="favorsetting"
-        >
-            <Stack.Screen name="root" component={DrawerNav} 
-                options={{headerShown:false}}
-            />
-            <Stack.Screen name="favorsetting0" component={FavorSetting0} 
-                options={{headerShown:false}}
-            />
-            <Stack.Screen name="favorsetting" component={FavorSetting} />
-            <Stack.Screen name="favorsetting2" component={FavorSetting2} />
-        </Stack.Navigator>
-    );
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        header: () => null,
+        gestureEnabled: false,
+      }}
+      initialRouteName="favorsetting"
+    >
+      <Stack.Screen
+        name="root"
+        component={Navbar}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="favorsetting0"
+        component={FavorSetting0}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen name="favorsetting" component={FavorSetting} />
+      <Stack.Screen name="favorsetting2" component={FavorSetting2} />
+    </Stack.Navigator>
+  );
 };
 
 export default FavorSettingStack;
